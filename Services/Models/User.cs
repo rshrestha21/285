560430f6d60da89e3a1c71f331b055a21d384c1c
Rajeev <rajeev.lama@selu.edu>
2018-10-14 20:37:17 -0500

    changes

--- Services/Models/User.cs
+++ Services/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,26 +10,38 @@ namespace Services.Models
     {
         public int Id { get; set; }
 
-       
 
-        public string FName { get; set; }
+        [Required(ErrorMessage = "First Name is required.")]
+        public string FirstName { get; set; }
 
-        public string LName{ get; set; }
+        [Required(ErrorMessage = "Last name is required.")]
+        public string LastName { get; set; }
 
-
+        [Required(ErrorMessage = "Username is required.")]
         public string Username { get; set; }
 
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
-        public string Gender { get; set; }
+        [RegularExpression(@"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "Please enter valid email . ")]
+        [Required(ErrorMessage = "Email is required.")]
+        public string Address { get; set; }
 
-        public string Address { get; set;}
+        [Compare("Password", ErrorMessage = "Please confirm your password.")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
 
+        [Required(ErrorMessage = "Phonenumber is required.")]
         public int Phonenumber { get; set; }
 
+        [Required(ErrorMessage = "Date of Birth is required.")]
         public DateTime DateofBirth { get; set; }
 
         public ICollection<Service> Services { get; set; }
+
+
+
     }
 
 }
